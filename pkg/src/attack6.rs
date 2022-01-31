70d31804f7ac02d9ca2b23c0efded3dc837ef1e9
Jeremy Banks <JBanks@users.noreply.github.com>
2022-01-30 20:05:43 -0500

    Attack 6
    Initial code for attack 6

---
+++ pkg/src/attack6.rs
@@ -0,0 +1,116 @@
+use crate::sys::{
+    DefaultEventHandler, DefaultScheduler, Device, ErrMsg, EventHandler, Mode, Router, System,
+    Word, WRD_EMPTY,
+};
+
+#[derive(Clone, Debug)]
+pub struct FakeStatusTrcmd {
+    pub attack_times: Vec<u128>,
+    pub success: bool,
+    pub word_count: u8,
+    pub flag: u8,
+    pub target: u8,         // the target RT
+    pub target_found: bool, // target found in traffic
+}
+
+impl FakeStatusTrcmd {
+    fn fake_status(&mut self, d: &mut Device) {
+        self.attack_times.push(d.clock.elapsed().as_nanos());
+        let w = Word::new_status(self.target);
+        d.write(w);
+        d.log(
+            WRD_EMPTY,
+            ErrMsg::MsgAttk(format!("Fake status injected!").to_string()),
+        );
+        self.target_found = false;
+        self.success = true;
+    }
+}
+
+impl EventHandler for FakeStatusTrcmd {
+    fn on_cmd(&mut self, d: &mut Device, w: &mut Word) {
+        // This function replaces "find_RT_tcmd" and "find_RT_rcmd" from Michael's code
+        // We cannot use on_cmd_trx here because that only fires after on_cmd verifies that the address is correct.
+        let destination = w.address();
+        self.word_count = w.dword_count();
+        if destination == self.target && self.target_found==false { // do we need the sub address?
+            if self.flag == 0 {
+                let new_flag;
+                if w.tr() == 1 {
+                    new_flag = 2;
+                    self.word_count = w.dword_count();
+                } else {
+                    new_flag = 1;
+                }
+                self.flag = new_flag;
+            }
+            if w.tr() == 0 {
+                self.word_count = w.dword_count();
+            }
+            self.target_found = true;
+            d.log(
+                *w, 
+                ErrMsg::MsgAttk(format!("Attacker>> Target detected(RT{})", self.target).to_string()),
+            );
+            d.log(
+                WRD_EMPTY, 
+                ErrMsg::MsgAttk(format!("Sending fake status word (after tr_cmd_word)").to_string()),
+            );
+            self.fake_status(d);
+        }
+        self.default_on_cmd(d, w);
+    }
+}
+
+pub fn test_attack6() {
+    // let mut delays_single = Vec::new();
+    let n_devices = 8;
+    // normal device has 4ns delays (while attacker has zero)
+    let w_delays = 4000;
+    let mut sys = System::new(n_devices as u32, w_delays);
+
+    // the last device is kept for attacker
+    for m in 0..n_devices - 1 {
+        let default_router = Router {
+            // control all communications (bc only)
+            scheduler: DefaultScheduler {
+                total_device: n_devices - 1,
+                target: 0,
+                data: vec![1, 2, 3],
+                proto: 0,
+            },
+            // control device-level response
+            handler: DefaultEventHandler {},
+        };
+
+        if m == 0 {
+            sys.run_d(m as u8, Mode::BC, default_router, false, 0);
+        } else {
+            sys.run_d(m as u8, Mode::RT, default_router, false, 0);
+        }
+    }
+    let attacker_router = Router {
+        // control all communications (bc only)
+        scheduler: DefaultScheduler {
+            total_device: n_devices - 1,
+            target: 0,
+            data: vec![1, 2, 3],
+            proto: 0,
+        },
+        // control device-level response
+        handler: FakeStatusTrcmd {
+            attack_times: Vec::new(),
+            word_count: 0u8,
+            success: false,
+            flag: 0,
+            target: 4, // attacking RT address @5
+            target_found: false,
+        },
+    };
+
+    sys.run_d(n_devices - 1, Mode::RT, attacker_router, false, 0);
+    sys.go();
+    sys.sleep_ms(10);
+    sys.stop();
+    sys.join();
+}
--- pkg/src/main.rs
+++ pkg/src/main.rs
@@ -1,15 +1,24 @@
 mod attack0;
 mod attack1;
+mod attack6;
+mod attack7;
+mod attack8;
 mod attack9;
 mod sys;
 use attack0::test_attack0;
 use attack1::test_attack1;
+use attack6::test_attack6;
+use attack7::test_attack7;
+use attack8::test_attack8;
 use attack9::test_attack9;
 use sys::test_default;
 
 fn main() {
-    test_default();
-    test_attack0();
-    test_attack1();
-    test_attack9();
+    // test_default();
+    // test_attack0();
+    // test_attack1();
+    test_attack6();
+    // test_attack7();
+    // test_attack8();
+    // test_attack9();
 }
