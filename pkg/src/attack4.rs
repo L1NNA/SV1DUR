e5a8f8610eeaeeb6f07f570598c41420c9687fe4
Jeremy Banks <JBanks@users.noreply.github.com>
2022-01-30 22:21:11 -0500

    Add 3 and 4
    Initial code for attacks 3 and 4.
    Attack 3 may have problems.  It only happens once right now.
    
    Attack 4 May also have problems.  It's untested at this time because I want to confirm the logic behind the code.

---
+++ pkg/src/attack4.rs
@@ -0,0 +1,111 @@
+use crate::sys::{
+    DefaultEventHandler, DefaultScheduler, Device, ErrMsg, EventHandler, Mode, Router, System,
+    Word, WRD_EMPTY, State,
+};
+
+#[derive(Clone, Debug)]
+pub struct ShutdownAttackRT {
+    pub attack_times: Vec<u128>,
+    pub success: bool,
+    pub word_count: u8,
+    pub flag: u8,
+    pub target: u8,         // the target RT
+    pub target_found: bool, // target found in traffic
+    pub destination: u8,
+}
+
+impl ShutdownAttackRT {
+    fn kill_rt(&mut self, d: &mut Device) {
+        d.log(
+            WRD_EMPTY,
+            ErrMsg::MsgAttk(format!("Attacker>> Killing RT{}", self.target).to_string()),
+        );
+        let word_count = 4;
+        let tr = 1;
+        self.attack_times.push(d.clock.elapsed().as_nanos());
+        let w = Word::new_cmd(self.target, word_count, tr);
+        d.write(w);
+        self.success = true;
+        d.set_state(State::Off); // Not sure what's going on here yet.  TODO come back to this.
+    }
+}
+
+impl EventHandler for ShutdownAttackRT {
+    fn on_cmd(&mut self, d: &mut Device, w: &mut Word) {
+        if w.address() == self.target && self.target_found == false {
+            d.log(
+                WRD_EMPTY,
+                ErrMsg::MsgAttk(format!("Attacker>> Killing RT{}", self.target).to_string()),
+            );
+            self.target_found = true;
+            self.kill_rt(d);
+        }
+        self.default_on_cmd(d, w);
+    }
+
+    fn on_sts(&mut self, d: &mut Device, w: &mut Word) {
+        if w.address() == self.target && self.target_found == false {
+            d.log(
+                WRD_EMPTY,
+                ErrMsg::MsgAttk(format!("Attacker>> Killing RT{}", self.target).to_string()),
+            );
+            self.target_found = true;
+            self.kill_rt(d);
+        }
+        self.default_on_sts(d, w);
+    }
+}
+
+pub fn test_attack4() {
+    // let mut delays_single = Vec::new();
+    let n_devices = 8;
+    // normal device has 4ns delays (while attacker has zero)
+    let w_delays = 4000;
+    let mut sys = System::new(n_devices as u32, w_delays);
+
+    // the last device is kept for attacker
+    for m in 0..n_devices - 1 {
+        let default_router = Router {
+            // control all communications (bc only)
+            scheduler: DefaultScheduler {
+                total_device: n_devices - 1,
+                target: 0,
+                data: vec![1, 2, 3],
+                proto: 0,
+            },
+            // control device-level response
+            handler: DefaultEventHandler {},
+        };
+
+        if m == 0 {
+            sys.run_d(m as u8, Mode::BC, default_router, false, 0);
+        } else {
+            sys.run_d(m as u8, Mode::RT, default_router, false, 0);
+        }
+    }
+    let attacker_router = Router {
+        // control all communications (bc only)
+        scheduler: DefaultScheduler {
+            total_device: n_devices - 1,
+            target: 0,
+            data: vec![1, 2, 3],
+            proto: 0,
+        },
+        // control device-level response
+        handler: ShutdownAttackRT {
+            attack_times: Vec::new(),
+            word_count: 0u8,
+            success: false,
+            flag: 0,
+            target: 4, // attacking RT address @5
+            target_found: false,
+            destination: 0u8,
+        },
+    };
+
+    sys.run_d(n_devices - 1, Mode::RT, attacker_router, false, 0);
+    sys.go();
+    sys.sleep_ms(10);
+    sys.stop();
+    sys.join();
+}
--- pkg/src/main.rs
+++ pkg/src/main.rs
@@ -1,5 +1,7 @@
 mod attack0;
 mod attack1;
+mod attack3;
+mod attack4;
 mod attack5;
 mod attack6;
 mod attack7;
@@ -8,6 +10,8 @@ mod attack9;
 mod sys;
 use attack0::test_attack0;
 use attack1::test_attack1;
+use attack3::test_attack3;
+use attack4::test_attack4;
 use attack5::test_attack5;
 use attack6::test_attack6;
 use attack7::test_attack7;
@@ -19,7 +23,9 @@ fn main() {
     // test_default();
     // test_attack0();
     // test_attack1();
-    test_attack5();
+    test_attack3();
+    // test_attack4();
+    // test_attack5();
     // test_attack6();
     // test_attack7();
     // test_attack8();
