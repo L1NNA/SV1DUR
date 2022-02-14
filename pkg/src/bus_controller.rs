4cd7400db906ed9dcfdca0c85443a3e3047cc1f7
Jeremy Banks <JBanks@users.noreply.github.com>
2022-02-13 20:53:44 -0500

    Initial bus controller code

---
+++ pkg/src/bus_controller.rs
@@ -0,0 +1,293 @@
+#[allow(unused_imports)]
+use crate::sys::{
+    DefaultEventHandler, DefaultScheduler, Device, ErrMsg, EventHandler, Mode, Router, System,
+    Word, WRD_EMPTY
+};
+#[allow(unused_imports)]
+use std::time::{Instant, Duration};
+use priority_queue::DoublePriorityQueue;
+
+#[allow(unused)]
+pub const ALERT_ON_COMPETING_CONTROLLER: bool = false; //if there is a competing bus controller, raise an alert;
+#[allow(unused)]
+pub const ALERT_ON_UNPROMPTED_DATA: bool = false; // Alert if we see a data word that we didn't solicit
+#[allow(unused)]
+pub const ALERT_ON_UNPROMPTED_STATUS: bool = false; // Alert if we see a status word that we didn't solicit
+#[allow(unused)]
+pub const ALERT_INTERUPTED_BEFORE_TRANSMIT: bool = false; // Alert if another RT sent data on the bus when we were asked to
+
+#[allow(unused)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Address {
+    BusControl,
+    FlightControls,
+    Trim,
+    Engine,
+    Flaps,
+    LandingGear,
+    Weapons,
+    Rudder,
+    Ailerons,
+    Elevators,
+    Slats,
+    Spoilers,
+    Brakes,
+    Fuel,
+    Heading,
+    Altimeter,
+    Positioning, //GPS
+    Pitch,
+}
+
+#[allow(unused)]
+impl Address {
+    pub fn priority(&self, destination: &Address) -> MsgPri {
+        match (self, destination) {
+            // With Feedback
+            (Address::FlightControls,   Address::Trim)              => MsgPri::Low,
+            (Address::Trim,             Address::FlightControls)    => MsgPri::Lowest,
+            (Address::FlightControls,   Address::Flaps)             => MsgPri::Low,
+            (Address::Flaps,            Address::FlightControls)    => MsgPri::Lowest,
+            (Address::FlightControls,   Address::Engine)            => MsgPri::VeryHigh,
+            (Address::Engine,           Address::FlightControls)    => MsgPri::High,
+            (Address::FlightControls,   Address::LandingGear)       => MsgPri::Low,
+            (Address::LandingGear,      Address::FlightControls)    => MsgPri::Lowest,
+            (Address::FlightControls,   Address::Weapons)           => MsgPri::VeryHigh,
+            (Address::Weapons,          Address::FlightControls)    => MsgPri::Medium,
+            // Without Feedback
+            (Address::FlightControls, Address::Rudder)      => MsgPri::VeryHigh,
+            (Address::FlightControls, Address::Ailerons)    => MsgPri::VeryHigh,
+            (Address::FlightControls, Address::Elevators)   => MsgPri::VeryHigh,
+            (Address::FlightControls, Address::Slats)       => MsgPri::VeryHigh,
+            (Address::FlightControls, Address::Spoilers)    => MsgPri::VeryHigh,
+            (Address::FlightControls, Address::Brakes)      => MsgPri::High,
+            //Sensors
+            (Address::Fuel,         Address::FlightControls) => MsgPri::Lowest,
+            (Address::Heading,      Address::FlightControls) => MsgPri::Medium,
+            (Address::Altimeter,    Address::FlightControls) => MsgPri::Medium,
+            (Address::Positioning,  Address::FlightControls) => MsgPri::Lowest,
+            (Address::Pitch,        Address::FlightControls) => MsgPri::Medium,
+            _ => MsgPri::VeryHigh,
+        }
+    }
+
+    pub fn repeat_function(&self, destination: &Address) -> bool {
+        match (self, destination) {
+            // With Feedback
+            (Address::FlightControls,   Address::Trim)              => true,
+            (Address::Trim,             Address::FlightControls)    => true,
+            (Address::FlightControls,   Address::Flaps)             => true,
+            (Address::Flaps,            Address::FlightControls)    => true,
+            (Address::FlightControls,   Address::Engine)            => true,
+            (Address::Engine,           Address::FlightControls)    => true,
+            (Address::FlightControls,   Address::LandingGear)       => true,
+            (Address::LandingGear,      Address::FlightControls)    => true,
+            (Address::FlightControls,   Address::Weapons)           => true,
+            (Address::Weapons,          Address::FlightControls)    => true,
+            // Without Feedback
+            (Address::FlightControls, Address::Rudder)      => true,
+            (Address::FlightControls, Address::Ailerons)    => true,
+            (Address::FlightControls, Address::Elevators)   => true,
+            (Address::FlightControls, Address::Slats)       => true,
+            (Address::FlightControls, Address::Spoilers)    => true,
+            (Address::FlightControls, Address::Brakes)      => true,
+            //Sensors
+            (Address::Fuel,         Address::FlightControls) => true,
+            (Address::Heading,      Address::FlightControls) => true,
+            (Address::Altimeter,    Address::FlightControls) => true,
+            (Address::Positioning,  Address::FlightControls) => true,
+            (Address::Pitch,        Address::FlightControls) => true,
+            _ => false,
+        }
+    }
+
+    pub fn word_count(&self, destination: &Address) -> u8 {
+        match (self, destination) {
+            // With Feedback
+            (Address::FlightControls,   Address::Trim)              => 2, //one float32 should carry sufficient data
+            (Address::Trim,             Address::FlightControls)    => 2,
+            (Address::FlightControls,   Address::Flaps)             => 1, //A single u4 could do it, but we're going to send a whole word
+            (Address::Flaps,            Address::FlightControls)    => 1,
+            (Address::FlightControls,   Address::Engine)            => 8, //We'll estimate a float32 for each of the engines (up to four engines) and 2 words per float32
+            (Address::Engine,           Address::FlightControls)    => 8,
+            (Address::FlightControls,   Address::LandingGear)       => 1, //Binary message, but we'll send a whole word
+            (Address::LandingGear,      Address::FlightControls)    => 1,
+            (Address::FlightControls,   Address::Weapons)           => 4, //Targeting information along with the weapon selected and whether or not to open the compartment
+            (Address::Weapons,          Address::FlightControls)    => 20, //confirmation data of currently configured system
+            // Without Feedback
+            (Address::FlightControls, Address::Rudder)      => 2, //float32 for degree
+            (Address::FlightControls, Address::Ailerons)    => 4, //float32 for degree on each wing
+            (Address::FlightControls, Address::Elevators)   => 4, //float32 for degree on each wing
+            (Address::FlightControls, Address::Slats)       => 4, //float32 for degree on each wing
+            (Address::FlightControls, Address::Spoilers)    => 4, //float32 for degree on each wing
+            (Address::FlightControls, Address::Brakes)      => 4, //float32 for degree on each side
+            //Sensors
+            (Address::Fuel,         Address::FlightControls) => 4, //one float32 for quantity, one float32 for flow
+            (Address::Heading,      Address::FlightControls) => 2, //one float32 for heading
+            (Address::Altimeter,    Address::FlightControls) => 1,
+            (Address::Positioning,  Address::FlightControls) => 3, // Lat, Long, Alt
+            (Address::Pitch,        Address::FlightControls) => 6, //float32 for pitch, bank, and roll
+            _ => 2, //2 words for anything unlisted
+        }
+    }
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub enum MsgPri {
+    Immediate,
+    VeryHigh,
+    High,
+    Medium,
+    Low,
+    VeryLow,
+    Lowest,
+}
+
+#[allow(unused)]
+impl MsgPri {
+    pub fn delay(&self) -> u32 {
+        // delays will be harmonic frequencies that double at each drop in priority
+        // 50Hz -- 1/50 = 0.02s -- 0.02 * 1000 * 1000 * 1000 = 20_000_000ns
+        match self {
+            MsgPri::Immediate   =>           0, // send this immediately
+            MsgPri::VeryHigh    =>  20_000_000, // 50Hz
+            MsgPri::High        =>  40_000_000, // 25Hz
+            MsgPri::Medium      =>  80_000_000, // 12.5Hz
+            MsgPri::Low         => 160_000_000, // 6.25Hz
+            MsgPri::VeryLow     => 320_000_000, // 3.125Hz
+            MsgPri::Lowest      => 640_000_000, // 1.5625Hz
+            _ => 0, // /infty Hz
+        }
+    }
+}
+
+pub struct HercScheduler { // Herc for Hercules which is a CC-130
+    priority_list: DoublePriorityQueue<(Address, Address), u128>, // instantiate with <(Address, Address)>
+    #[allow(unused)]
+    timeout: u128,
+}
+
+impl HercScheduler {
+    pub fn new() -> Self {
+        let mut scheduler = HercScheduler{
+            priority_list: DoublePriorityQueue::new(),
+            timeout: 0,
+        };
+        scheduler.priority_list.push((Address::FlightControls, Address::Trim), 0);
+        scheduler.priority_list.push((Address::FlightControls, Address::Ailerons), 0);
+        scheduler.priority_list.push((Address::FlightControls, Address::Elevators), 0);
+        scheduler.priority_list.push((Address::FlightControls, Address::Engine), 0);
+        scheduler.priority_list.push((Address::Engine, Address::FlightControls), 0);
+        scheduler.priority_list.push((Address::LandingGear, Address::FlightControls), 0);
+        return scheduler;
+    }
+
+    pub fn on_bc_ready(&mut self, d: &mut Device) {
+        // The current setup does not allow for a message to be prepended to the list.
+        // We pop the next message and wait until we should send it.  
+        // If a message is prepended it will be sent immediately after the first one is delivered.
+        let spin_sleeper = spin_sleep::SpinSleeper::new(1000);
+        let message = self.priority_list.pop_min();
+        match message {
+            Some(((src, dst), mut time)) => { 
+                let wc = src.word_count(&dst);
+                time = if time > self.timeout {time} else {self.timeout};
+                if time > d.clock.elapsed().as_nanos() {
+                    //TODO account for possible overflow if we're very close to execution time.
+                    let wait = time - d.clock.elapsed().as_nanos();
+                    spin_sleeper.sleep_ns(wait.try_into().unwrap());
+                }
+                let current = d.clock.elapsed().as_nanos();
+                match (src, dst) {
+                    (source, _) if source as u8 == d.address => {// BC to RT
+                        if src.repeat_function(&dst) {
+                            self.update_priority((src, dst), time);
+                        }
+                        // d.act_bc2rt(dst as u8, wc); // Can't be wordcount, must be data
+                        println!("[{current:0>10?}] from {src:?} to {dst:?} with {src:?} as BC");
+                        let bus_available = current + (2+wc as u128) * 20_000;
+                        println!("[{:0>10?}] - message finished", bus_available);
+                        self.timeout = bus_available;
+                    }
+                    (_, destination) if destination as u8 == d.address => {// RT to BC
+                        if src.repeat_function(&dst) {
+                            self.update_priority((src, dst), time);
+                        }
+                        d.act_rt2bc(src as u8, wc);
+                        println!("[{current:0>10?}] from {src:?} to {dst:?} with {dst:?} as BC");
+                        let bus_available = current + (2+wc as u128) * 20_000;
+                        println!("[{:0>10?}] - message finished", bus_available);
+                        self.timeout = bus_available;
+                    }
+                    _ => {// RT to RT
+                        if src.repeat_function(&dst) {
+                            self.update_priority((src, dst), time);
+                        }
+                        d.act_rt2rt(src as u8, dst as u8, wc);
+                        println!("[{current:0>10?}] from {src:?} to {dst:?}");
+                        let bus_available = current + (4+wc as u128) * 20_000;
+                        println!("[{:0>10?}] - message finished", bus_available);
+                        self.timeout = bus_available;
+                    }
+                }
+            }
+            None => {}
+        }
+        // self.timeout = bus_available; // d.clock.elapsed().as_nanos() + 20_000 + 16_000; // 20us for word transmission and 16us for timeout between messages
+    }
+
+    pub fn update_priority(&mut self, pairing: (Address, Address), time: u128) {
+        let (src, dst) = pairing;
+        let delay = src.priority(&dst).delay() as u128;
+        let next_time = time + delay;
+        // println!("Appended {pairing:?} at time: {next_time:?} - {time}+{delay}");
+        self.priority_list.push(pairing, next_time);
+    }
+}
+
+// pub struct BCHandler {
+//     address: Address,
+//     priority_list: PriorityQueue, //A container to select the next device to communicate, and when that device should send its data
+//     timeout: u128,
+// }
+
+// impl EventHandler for BCHandler {
+//     fn on_cmd(&mut self, d: &mut Device, w: &mut Word) {
+//         if ALERT_ON_COMPETING_CONTROLLER {
+//             //Another terminal is trying to act as the bus controller.
+//         }
+//         self.default_on_cmd(d, w)
+//     }
+
+//     fn on_dat(&mut self, d: &mut Device, w: &mut Word) {
+//         // count the number of data words so we know when the current message is finished.
+//         if d.state == State::AwtData {
+//             // perform any processing on this data word
+//         } else if ALERT_ON_UNPROMPTED_DATA {
+//             // We didn't ask for this many data words
+//         } else {
+//             // ignore the data word
+//         }
+//         self.default_on_dat(d, w)
+//     }
+
+//     fn on_sts(&mut self, d: &mut Device, w: &mut Word) {
+//         // check for SR bit and include in the priority list.
+//         if State::AwtStsRcvB2R <= d.state <= State::AwtStsTrxR2R {
+//             if w.service_request_bit() {
+//                 // Add requested item to the queue
+//             }
+//         } else if ALERT_ON_UNPROMPTED_STATUS {
+//             // We didn't ask for this status word.
+//         } else {
+
+//         }
+//         self.default_on_sts(d, w)
+//     }
+
+//     fn on_err_parity(&mut self, d: &mut Device, w: &mut Word) {
+//         // We should implement an error correction mechanism here.
+//         self.default_on_err_parity(d, w)
+//     }
+// }
--- pkg/src/main.rs
+++ pkg/src/main.rs
@@ -5,19 +5,90 @@ use attacks::{
     test_attack1, test_attack10, test_attack2, test_attack3, test_attack4, test_attack5,
     test_attack6, test_attack7, test_attack8, test_attack9,
 };
+use crossbeam_channel::{bounded};
 #[allow(unused_imports)]
-use sys::test_default;
+use sys::{test_default, AttackType, Mode, State};
+use std::time::{Instant};
+mod bus_controller; //::{Address, MsgPri, HercScheduler};
+#[allow(unused_imports)]
+use bus_controller::{Address, HercScheduler};
+
+#[allow(unused)]
+fn test_address_functions() {
+    for (src, dst) in [
+            (Address::FlightControls,   Address::Trim),
+            (Address::Trim,             Address::FlightControls),
+            (Address::FlightControls,   Address::Flaps),
+            (Address::Flaps,            Address::FlightControls),
+            (Address::FlightControls,   Address::Engine),
+            (Address::Engine,           Address::FlightControls),
+            (Address::FlightControls,   Address::LandingGear),
+            (Address::LandingGear,      Address::FlightControls),
+            (Address::FlightControls,   Address::Weapons),
+            (Address::Weapons,          Address::FlightControls),
+            // Without Feedback
+            (Address::FlightControls, Address::Rudder),
+            (Address::FlightControls, Address::Ailerons),
+            (Address::FlightControls, Address::Elevators),
+            (Address::FlightControls, Address::Slats),
+            (Address::FlightControls, Address::Spoilers),
+            (Address::FlightControls, Address::Brakes),
+            //Sensors
+            (Address::Fuel,         Address::FlightControls),
+            (Address::Heading,      Address::FlightControls),
+            (Address::Altimeter,    Address::FlightControls),
+            (Address::Positioning,  Address::FlightControls),
+            (Address::Pitch,        Address::FlightControls),
+        ] {
+        println!("src: {:?}, dst: {:?}", src, dst);
+        println!("priority: {:?}", src.priority(&dst));
+        println!("repeating: {:?}", src.repeat_function(&dst));
+        if src.repeat_function(&dst) {
+            println!("Repeat freq: {:?}Hz", 1.0/(src.priority(&dst).delay() as f32/1_000_000_000.0));
+        }
+        println!("word_count: {:?}", src.word_count(&dst));
+        println!("------------------")
+    }
+}
+
+fn test_herc_scheduler() {
+    let (_trans, recv) = bounded(512);
+    let mut bc = sys::Device{
+        fake: false,
+        atk_type: AttackType::Benign,
+        ccmd: 0,
+        mode: Mode::BC,
+        state: State::Idle,
+        memory: Vec::new(),
+        number_of_current_cmd: 0,
+        in_brdcst: false,
+        address: Address::Engine as u8,
+        id: Address::Engine as u32,
+        dword_count: 0,
+        dword_count_expected: 0,
+        clock: Instant::now(),
+        logs: Vec::new(),
+        transmitters: Vec::new(),
+        read_queue: Vec::new(),
+        write_queue: Vec::new(),
+        write_delays: 0,
+        receiver: recv,
+        delta_t_avg: 0,
+        delta_t_start: 0,
+        delta_t_count: 0,
+    };
+    let mut scheduler = HercScheduler::new();
+    for _ in 0..20 {
+        scheduler.on_bc_ready(&mut bc);
+    }
+    // build herc scheduler
+    // run scheduler for about 1 second
+    // see how many times each element was acted on
+}
 
 fn main() {
-    // test_default();
-    // test_attack0();
-    // test_attack1();
-    test_attack2();
-    // test_attack3();
-    // test_attack4();
-    // test_attack5();
-    // test_attack6();
-    // test_attack7();
-    // test_attack8();
-    // test_attack9();
+    // test_address_functions();
+    
+    test_herc_scheduler();
+
 }
