7e49a9d69b59dfd22506c4fccd4a287f6cdd5985
Ryan Kerr <15rhk@queensu.ca>
2022-03-09 10:39:03 -0500

    Almost working

--- flight_data_recorder/src/main.rs
+++ flight_data_recorder/src/main.rs
@@ -1,4 +1,7 @@
+// vim: cc=100
+
 use simconnect;
+use sqlite;
 use std::time::Duration;
 use std::thread::sleep;
 use std::mem::transmute_copy;
@@ -19,9 +22,20 @@ const SIM_FRAME: ScPeriod =
 const SECOND: ScPeriod =
     simconnect::SIMCONNECT_PERIOD_SIMCONNECT_PERIOD_SECOND;
 
+type Boolean = bool;
+type Integer = i64;
+type Float = f64;
+
+#[repr(u32)]
+enum SystemEvent {
+    Sim,
+    Paused,
+}
+
 // Unit Enumerations
 macro_rules! define_units {
     ( $( $unit:ident : $type:ident = $string:literal ),+ , ) => {
+
         #[allow(unused)]
         #[derive(Debug)]
         enum Unit {
@@ -50,6 +64,8 @@ macro_rules! define_units {
 }
 
 define_units! {
+    Bool: INT64 = "Bool",
+
     Hours: INT64 = "hours",
     Minutes: INT64 = "minutes",
     Seconds: INT64 = "seconds",
@@ -80,114 +96,207 @@ define_units! {
 macro_rules! define_sensors {
     ( @1 $_:tt ) => (1);
 
-    ( $( $name:ident <- $datum_name:literal : $datum_type:ident, $type:ty );*; ) => {
+    ( @column $name:ident $type:ty ) => {
+        concat!(stringify!($name), " ", stringify!($type))
+    };
+
+    ( @column $name:ident $type:ty , $($tail:tt)+ ) => {
+        concat! {
+            define_sensors!(@column $name $type), ", ",
+            define_sensors!(@column $($tail)+)
+        }
+    };
+
+    ( @insert $name:ident ) => {
+        concat!(":", stringify!($name))
+    };
+
+    ( @insert $name:ident , $($names:ident),+ ) => {
+        concat! {
+            define_sensors!(@insert $name), ", ",
+            define_sensors!(@insert $($names),+)
+        }
+    };
+
+    ( $( $name:ident
+         <- $datum_name:literal
+         in $datum_type:ident
+         as $type:ty
+      );+; ) => {
 
         #[allow(unused)]
         #[derive(Debug)]
         struct SensorData {
-            $( $name : $type ),*
+            $( $name : $type ),+
         }
 
-        const SENSORS: [(&str, Unit); 0 $(+ define_sensors!(@1 $name))*] = [
-            $( ( $datum_name, $datum_type ) ),*
-        ];
+        impl SensorData {
+            const SENSORS: [(&'static str, Unit); 0 $(+ define_sensors!(@1 $name))+] = [
+                $( ( $datum_name, $datum_type ) ),+
+            ];
+
+            const SQL_CREATE_TABLE_STATEMENT: &'static str = concat! {
+                "create table sensor_data (",
+                    define_sensors!(@column $( $name $type ),+),
+                ")"
+            };
+
+            const SQL_INSERT_STATEMENT: &'static str = concat! {
+                "insert into sensor_data values (",
+                    define_sensors!(@insert $( $name ),+),
+                ")"
+            };
+
+            #[inline(always)]
+            fn persist(&self, statement: &mut sqlite::Statement) -> sqlite::Result<()> {
+                $(
+                    statement.bind_by_name(
+                        define_sensors!(@insert $name), self.$name
+                    )?;
+                )+
+                Ok(())
+            }
+        }
     };
 }
 
 define_sensors! {
     // TIME
-    absolute_time <- "ABSOLUTE TIME" : Seconds, i64;
+    absolute_time <- "ABSOLUTE TIME" in Seconds as Integer;
 
     // INSTRUMENT CLUSTER
-    indicated_airspeed <- "AIRSPEED INDICATED" : Knots, f64;
-    indicated_altitude <- "INDICATED ALTITUDE" : Feet, f64;
-    vertical_speed <- "VERTICAL SPEED" : FeetPerMinute, f64;
-    heading_indicator <- "HEADING INDICATOR" : Radians, f64;
-    plane_heading_gyro <- "PLANE HEADING DEGREES GYRO" : Radians, f64;
-    wiskey_compass_indicaton <- "WISKEY COMPASS INDICATION DEGREES" : Degrees, f64;
-    angle_of_attack_indicator <- "ANGLE OF ATTACK INDICATOR" : Radians, f64;
+    indicated_airspeed <- "AIRSPEED INDICATED" in Knots as Float;
+    indicated_altitude <- "INDICATED ALTITUDE" in Feet as Float;
+    vertical_speed <- "VERTICAL SPEED" in FeetPerMinute as Float;
+    heading_indicator <- "HEADING INDICATOR" in Radians as Float;
+    plane_heading_gyro <- "PLANE HEADING DEGREES GYRO" in Radians as Float;
+    wiskey_compass_indicaton <- "WISKEY COMPASS INDICATION DEGREES" in Degrees as Float;
+    angle_of_attack_indicator <- "ANGLE OF ATTACK INDICATOR" in Radians as Float;
 
-    fuel_total_quantity <- "FUEL TOTAL QUANTITY" : Gallons, f64;
-    estimated_fuel_flow <- "ESTIMATED FUEL FLOW" : PoundsPerHour, f64;
+    fuel_total_quantity <- "FUEL TOTAL QUANTITY" in Gallons as Float;
+    estimated_fuel_flow <- "ESTIMATED FUEL FLOW" in PoundsPerHour as Float;
 
     // SPEED DATA (WORLD)
-    ground_velocity <- "GROUND VELOCITY" : Knots, f64;
-    total_world_velocity <- "TOTAL WORLD VELOCITY" : FeetPerSecond, f64;
+    ground_velocity <- "GROUND VELOCITY" in Knots as Float;
+    total_world_velocity <- "TOTAL WORLD VELOCITY" in FeetPerSecond as Float;
 
-    velocity_world_x <- "VELOCITY WORLD X" : FeetPerSecond, f64;
-    velocity_world_y <- "VELOCITY WORLD Y" : FeetPerSecond, f64;
-    velocity_world_z <- "VELOCITY WORLD Z" : FeetPerSecond, f64;
+    velocity_world_x <- "VELOCITY WORLD X" in FeetPerSecond as Float;
+    velocity_world_y <- "VELOCITY WORLD Y" in FeetPerSecond as Float;
+    velocity_world_z <- "VELOCITY WORLD Z" in FeetPerSecond as Float;
 
-    acceleration_world_x <- "ACCELERATION WORLD X" : FeetPerSecondSquared, f64;
-    acceleration_world_y <- "ACCELERATION WORLD Y" : FeetPerSecondSquared, f64;
-    acceleration_world_z <- "ACCELERATION WORLD Z" : FeetPerSecondSquared, f64;
+    acceleration_world_x <- "ACCELERATION WORLD X" in FeetPerSecondSquared as Float;
+    acceleration_world_y <- "ACCELERATION WORLD Y" in FeetPerSecondSquared as Float;
+    acceleration_world_z <- "ACCELERATION WORLD Z" in FeetPerSecondSquared as Float;
 
     // SPEED DATA (PLANE)
-    velocity_plane_x <- "VELOCITY BODY X" : FeetPerSecond, f64;
-    velocity_plane_y <- "VELOCITY BODY Y" : FeetPerSecond, f64;
-    velocity_plane_z <- "VELOCITY BODY Z" : FeetPerSecond, f64;
+    velocity_plane_x <- "VELOCITY BODY X" in FeetPerSecond as Float;
+    velocity_plane_y <- "VELOCITY BODY Y" in FeetPerSecond as Float;
+    velocity_plane_z <- "VELOCITY BODY Z" in FeetPerSecond as Float;
 
-    acceleration_plane_x <- "ACCELERATION BODY X" : FeetPerSecondSquared, f64;
-    acceleration_plane_y <- "ACCELERATION BODY Y" : FeetPerSecondSquared, f64;
-    acceleration_plane_z <- "ACCELERATION BODY Z" : FeetPerSecondSquared, f64;
+    acceleration_plane_x <- "ACCELERATION BODY X" in FeetPerSecondSquared as Float;
+    acceleration_plane_y <- "ACCELERATION BODY Y" in FeetPerSecondSquared as Float;
+    acceleration_plane_z <- "ACCELERATION BODY Z" in FeetPerSecondSquared as Float;
 
     // ANGLE OF ATTACK
-    plane_pitch <- "PLANE PITCH DEGREES" : Radians, f64; // "Degrees"
-    plane_bank <- "PLANE BANK DEGREES" : Radians, f64; // "Degrees"
+    plane_pitch <- "PLANE PITCH DEGREES" in  Radians as Float; // "Degrees"
+    plane_bank <- "PLANE BANK DEGREES" in Radians as Float; // "Degrees"
 
-    incidence_alpha <- "INCIDENCE ALPHA" : Radians, f64; // AoA
-    incidence_beta <- "INCIDENCE BETA" : Radians, f64; // Sideslip
+    incidence_alpha <- "INCIDENCE ALPHA" in Radians as Float; // AoA
+    incidence_beta <- "INCIDENCE BETA" in Radians as Float; // Sideslip
 
     // GPS DATA
-    gps_latitude <- "GPS POSITION LAT" : Degrees, f64;
-    gps_longitude <- "GPS POSITION LON" : Degrees, f64;
-    gps_altitude <- "GPS POSITION ALT" : Meters, f64;
+    gps_latitude <- "GPS POSITION LAT" in Degrees as Float;
+    gps_longitude <- "GPS POSITION LON" in Degrees as Float;
+    gps_altitude <- "GPS POSITION ALT" in Meters as Float;
 
-    plane_latitude <- "PLANE LATITUDE" : Degrees, f64;
-    plane_longitude <- "PLANE LONGITUDE" : Degrees, f64;
-    plane_altitude <- "PLANE ALTITUDE" : Feet, f64;
+    plane_latitude <- "PLANE LATITUDE" in Degrees as Float;
+    plane_longitude <- "PLANE LONGITUDE" in Degrees as Float;
+    plane_altitude <- "PLANE ALTITUDE" in Feet as Float;
 
     // WEATHER DATA
-    ambient_temperature <- "AMBIENT TEMPERATURE" : Celsius, f64;
-    ambient_pressure <- "AMBIENT PRESSURE" : SlugsPerCubicFoot, f64;
-    ambient_wind_velocity <- "AMBIENT WIND VELOCITY" : Knots, f64;
-    ambient_wind_direction <- "AMBIENT WIND DIRECTION" : Degrees, f64;
-    ambient_wind_x <- "AMBIENT WIND X" : MetersPerSecond, f64;
-    ambient_wind_y <- "AMBIENT WIND Y" : MetersPerSecond, f64;
-    ambient_wind_z <- "AMBIENT WIND Z" : MetersPerSecond, f64;
-    total_air_temperature <- "TOTAL AIR TEMPERATURE" : Celsius, f64;
+    ambient_temperature <- "AMBIENT TEMPERATURE" in Celsius as Float;
+    ambient_pressure <- "AMBIENT PRESSURE" in SlugsPerCubicFoot as Float;
+    ambient_wind_velocity <- "AMBIENT WIND VELOCITY" in Knots as Float;
+    ambient_wind_direction <- "AMBIENT WIND DIRECTION" in Degrees as Float;
+    ambient_wind_x <- "AMBIENT WIND X" in MetersPerSecond as Float;
+    ambient_wind_y <- "AMBIENT WIND Y" in MetersPerSecond as Float;
+    ambient_wind_z <- "AMBIENT WIND Z" in MetersPerSecond as Float;
+    total_air_temperature <- "TOTAL AIR TEMPERATURE" in Celsius as Float;
 }
 
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn main() {
-    let mut conn = simconnect::SimConnector::new();
+fn main() -> Result<()> {
+    let mut sim_conn = simconnect::SimConnector::new();
 
-    if ! conn.connect("Demo") {
-        panic!("Failed to connect");
+    if ! sim_conn.connect("SV1DUR Flight Data Recorder") {
+        return Err("Failed to connect to flight simulator");
     }
 
-    for (key, unit) in SENSORS {
-        if ! conn.add_data_definition(0, key, unit.to_str(), unit.to_type(), u32::MAX) {
+    let dirname = chrono::Utc::now().format("%F-%H-%M-%S-%f").to_string();
+    std::fs::create_dir(std::path::PathBuf::from(&dirname))?;
+
+    let mut conn = sqlite::open(format!("{}/flight_data.sqlite", dirname))?;
+    conn.execute(SensorData::SQL_CREATE_TABLE_STATEMENT)?;
+
+    let mut stmt = conn.prepare(SensorData::SQL_INSERT_STATEMENT)?;
+
+    for (key, unit) in SensorData::SENSORS {
+        if ! sim_conn.add_data_definition(0, key, unit.to_str(), unit.to_type(), u32::MAX) {
             panic!("Invalid key: {}", key);
         }
     }
 
-    conn.request_data_on_sim_object(0, 0, 0, SECOND, 0, 0, 0, 0);
+    sim_conn.request_data_on_sim_object(0, 0, 0, SECOND, 0, 0, 0, 0);
+    sim_conn.subscribe_to_system_events(SystemEvent::Sim as u32, "Sim");
+    sim_conn.subscribe_to_system_events(SystemEvent::Paused as u32, "Paused");
+
+    let mut paused = true;
+    println!("-- -- -- -- -- -- -- --");
 
     loop {
-        match conn.get_next_message() {
-            Ok(simconnect::DispatchResult::SimobjectData(data)) => {
+        use simconnect::DispatchResult::*;
+        match sim_conn.get_next_message() {
+            Ok(SimobjectData(data)) => {
+                if paused {
+                    continue;
+                }
+
                 unsafe {
                     if data.dwDefineID == 0 {
                         let sim_data: SensorData = transmute_copy(&data.dwData);
-                        //println!("Indicated Airspeed: {:?}", sim_data.indicated_airspeed);
-                        println!("{:?}", sim_data);
+                        sim_data.persist(&stmt);
                     }
                 }
+            },
+            Ok(Event(event)) => {
+                unsafe {
+                    use SystemEvent::*;
+                    match event.uEventID {
+                        Sim => {
+                            paused = event.dwData == 0;
+
+                            if paused {
+                                println!("Simulation has stopped.");
+                                break;
+                            }
+                        },
+                        Paused => {
+                            paused = event.dwData == 1;
+                        },
+                        _ => ()
+                    }
+                }
+            },
+            Ok(Quit(_)) => {
+                println!("Flight Simulator has exited.");
+                break;
             },
             _ => ()
         }
 
         sleep(Duration::from_millis(16));
     }
+
+    Ok(())
 }
